8b9ee153a0c31aaaeaba961f52005f061d2012e2
Alessandro Antonio Sollar Junior <57305761+Juniorsollar@users.noreply.github.com>
2024-11-05 15:31:04 -0300

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ export default function Header() {
         <header>
             <div>
                 <img
-                src="https://media.licdn.com/dms/image/v2/C4E03AQFZUapwnxaV3w/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1654094228278?e=1730332800&v=beta&t=Pk6uqY4lHrTyzk0xYh6I9umvmKhuW_sTqLPCvRX2a2c"
+                src="https://media.licdn.com/dms/image/v2/C4E03AQFZUapwnxaV3w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1654094228278?e=1736380800&v=beta&t=8-cQLMa_pl3GtK0m8uRcWwcdXL9E9BDciRyK3bTI_xo"
                 alt="Foto de Alessandro Sollar em um congresso de Engenharia de Computação"/>
             </div>
         </header>
